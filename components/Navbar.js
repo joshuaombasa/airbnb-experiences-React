c8e9a6d3a0463c9a98156b240751703008ff45e5
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-26 23:47:00 +0300

    added script file

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Navbar() {
+    return (
+        <div className="navbar-container">
+           <img src="images/airbnb-logo.png" className="airbnb-logo" />
+        </div>
+    )
+}

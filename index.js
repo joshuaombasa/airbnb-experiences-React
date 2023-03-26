568c90fea4cdd2e0a20816877629ac0ea2001b17
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-26 23:45:32 +0300

    added script file

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import expeiencesData from './data.js'
+import App from './App'
+
+console.log(expeiencesData)
+ReactDOM.render(<App />, document.getElementById('root'))

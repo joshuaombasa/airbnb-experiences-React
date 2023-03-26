6584cb04226cdb1cd39b2cd610872ae3720128f3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-26 23:46:30 +0300

    added script file

---
+++ components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div className="hero-container">
+            <img  src="images/photo-grid.png"/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
